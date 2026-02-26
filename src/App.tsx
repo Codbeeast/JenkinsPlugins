f17ce1c3afa5fbeef9c5fdf022e7237d6eabfadd
Codbeeast <vishwakarmapriyanshu9099@gmail.com>
2026-02-26 22:26:45 +0530

    Fixed some issues

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import DataDictionary from './pages/DataDictionary';
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
                 <Route element={<Layout />}>
                     <Route path="/" element={<Overview />} />
